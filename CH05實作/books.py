dcf5e685b20140934377d662bc5911cd42d2e6ed
Rayder <zen2362661@gmail.com>
2022-02-20 00:43:24 +0800

    34

--- CH05實作/books.py
+++ CH05實作/books.py
@@ -1,4 +1,5 @@
 from matplotlib.pyplot import title
+from sympy import content
 
 
 def showbook(url,kind):
@@ -26,6 +27,26 @@ def showpage(url, kind):
         scr = item.select('a img')[0]["src"]
         title = msg.select('a')[0].text
         imgurl = scr.split("?i=")[-1].split("&")[0] #ex:https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/091/65/0010916571.jpg&v=6200f4bc&w=170&h=170
-        author = msg.
+        author = msg.select('a')[1].text
+        pulish = msg.select('a')[2].text
+        date = msg.find('span').text.split(":")[-1]
+        onsale = item.select('.price .set2')[0].text
+        content = item.select('.txt_cont')[0].text.replace(" ","").strip()
+
+        print("\n分類"+kind)
+        print("書名:"+title)
+        print("圖片網址:"+imgurl)
+        print("作者:"+author)
+        print("出版社:"+pulish)
+        print("出版日期:"+date)
+        print(onsale)
+        print("內容:"+content)
+        n+=1
+        print("n=",n)
+#        if n==2:break #開發用
+
+def twobyte(kindno):
+    if kindno<10:
+        kindnostr="0"+str(kindno)
 
 # https://www.books.com.tw/web/books_nbtopm_19/?v=1&o=5
